e902052aa1bb1c85ff9cadb3ac01724ed25d0a61
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-14 12:58:15 -0500

    add changes in file index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,31 @@
-#!/usr/bin/python3
-"""
-import app_views from api.v1.views
-"""
 from api.v1.views import app_views
 from flask import jsonify
-from flask import Flask
-from models import storage
-import json
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status',
+                 strict_slashes=False, methods=['GET'])
 def status():
-    """
-    Returns the status of the application
-    """
-    return jsonify({"status": "OK"})
+    """Returns OK if endpoint was correctly created"""
+    return jsonify({'status': 'OK'}), 200
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats',
+                 strict_slashes=False, methods=['GET'])
 def stats():
-    """
-    Returns the number of objects by type
-    """
-
+    """Returns number of each object by type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
     from models import storage
-    classes = {"amenities": storage.count('Amenity'),
-               "cities": storage.count('City'),
-               "places": storage.count('Place'),
-               "reviews": storage.count('Review'),
-               "states": storage.count('State'),
-               "users": storage.count('User')}
-    json_string = json.dumps(classes, indent=2)
-    return json_string
+
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    count = {}
+
+    for key, val in classes.items():
+        count[key] = storage.count(val)
+    return jsonify(count), 200
